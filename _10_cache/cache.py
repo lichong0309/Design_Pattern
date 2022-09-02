fc4b462f391169ddd0b94f71e30fd6e95cb34ea3
lichong0309 <chongli0309@gmail.com>
2022-09-02 14:41:02 +0800

    update

--- _10_cache/cache.py
+++ _10_cache/cache.py
@@ -1,57 +1,32 @@
-# 使用hash构造函数的属性来消除重复的实例
-
-
-# 抽象网站类
-class Website(object):
-
-    def use(self):
+class FlyweightBase:
+    """享元基类"""
+    def offer(self):
         pass
 
-# 具体网站类
-class ConcreteWebsite(Website):
 
+class Flyweight(FlyweightBase):
+    """共享享元类"""
     def __init__(self, name):
         self.name = name
 
-    def use(self):
-        print ("网站分类",self.name)
-# 不共享的网站类
-class UnshareConcreteWebsite(Website):
-    def __init__(self, name):
-        self.name = name
+    def get_price(self, price):
+        print('产品类型：{} 详情：{}'.format(self.name, price))
 
-    def use(self):
-        print ("不共享网站分类",self.name)
 
-# 网站工厂
-class WebsiteFactory(object):
-
+class FactoryFlyweight:
+    """享元工厂类"""
     def __init__(self):
-        self.hashtable = dict()
+        self.product = {}
 
-    # 获取网站类  如果存在直接返回,如果不存在建好之后返回
-    def get_website(self, key):
-        if not key in self.hashtable:
-            self.hashtable[key] = ConcreteWebsite(key)
-        return self.hashtable[key]
+    def Getproduct(self, key):
+        if not self.product.get(key, None):
+            self.product[key] = Flyweight(key)
+        return self.product[key]
 
-    # 网站实例的个数
-    def get_website_count(self):
-        return len(self.hashtable.keys())
 
-if __name__ == "__main__":
-    factory = WebsiteFactory()
-    f1 = factory.get_website("blog")
-    f2 = factory.get_website("blog")
-    f3 = factory.get_website("blog")
-    f4 = factory.get_website("website")
-    f5 = factory.get_website("website")
-    f6 = factory.get_website("website")
-    f7 = UnshareConcreteWebsite("test")
-    f1.use()
-    f2.use()
-    f3.use()
-    f4.use()
-    f5.use()
-    f6.use()
-    f7.use()
+if __name__ == '__main__':
+    test = FactoryFlyweight()
+    A = test.Getproduct("高端")
+    A.get_price("香水：80")
+    B = test.Getproduct("高端")
+    B.get_price("面膜：800")
